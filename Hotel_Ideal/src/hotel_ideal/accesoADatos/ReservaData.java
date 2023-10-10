0eac6898451f3e5345e64873751a47c4fd0793af
gustavotorres37 <141786618+gustavotorres37@users.noreply.github.com>
2023-10-10 17:56:42 -0300

    Metodo crearReserva ();

--- Hotel_Ideal/src/hotel_ideal/accesoADatos/ReservaData.java
+++ Hotel_Ideal/src/hotel_ideal/accesoADatos/ReservaData.java
@@ -5,12 +5,13 @@
  */
 package hotel_ideal.accesoADatos;
 
+import hotel_ideal.entidades.Reserva;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,44 +19,52 @@ import javax.swing.JOptionPane;
  * @author gtorre
  */
 public class ReservaData {
-    
+
     Connection con = Conexion.getConexion();
     
-    
-    
-    public void crearReserva (){
-        
-       String sql= "INSERT INTO reserva (idHabitacion ,idHuesped ,fechaInicio ,fechaFin ,precioTotal ,cantPersonas,cantidadDeDias,activo) VALUES (?,?,?,?,?,?,?,?)";
-       
-       PreparedStatement ps;
-        
-        
-        try {
-            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null," No se ha podido dar de alta la reserva ");
-        }
-        
-        
-        
-        
-        
-        
-        
-        
+    public ReservaData(){
         
     }
+
     
+
+    
+
+    public void crearReserva( Reserva reserva) {
+
+        String sql = "INSERT INTO reserva (idHabitacion,idHuesped,fechaInicio ,fechaFin,precioTotal,cantPersonas,cantidadDeDias,activo) VALUES (?,?,?,?,?,?,?,?)";
+
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps = con.prepareStatement(sql);
+            ps.setString(1, reserva.getHabitacion(getIdHabitacion()));
+            ps.setInt(2, reserva.getHuesped(getIdHuesped));
+            ps.setDate(3, Date.valueOf(reserva.getFechaInicio()));
+            ps.setDate(4, Date.valueOf(reserva.getFechaFin));
+            ps.setDouble(5, reserva.getPrecioFinal);
+            ps.setInt(6, reserva.getCantidadPersonas);
+            ps.setInt(7, reserva.getCantidadDeDias);
+            ps.setBoolean(8, isActivo());
+
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                reserva.setIdReserva(rs.getInt("idReserva"));
+
+                JOptionPane.showMessageDialog(null, "Reserva  concretada con exito");
+
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, " No se ha podido dar de alta la reserva ");
+        }
+
+    }
+
 }
